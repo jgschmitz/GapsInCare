d424e532a3761f55d613ecbba3e16c415cfa9fa4
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2025-05-19 14:19:27 -0500

    Update AbnormalAIC.js

--- AbnormalAIC.js
+++ AbnormalAIC.js
@@ -1,5 +1,4 @@
-db.gaps.find({
+db.getSiblingDB("UHG_Care").Gaps.find({
   "lab.loincCode": "4548-4",
-  "lab.testType": "Hemoglobin A1C",
-  "lab.result": { $gt: "8.0" }
+  "lab.result": { $gt: "8" }
 })
