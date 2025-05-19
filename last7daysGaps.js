adea278117fff53ffac65dce3fce9cff0d58dfe3
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2025-05-19 14:20:35 -0500

    Update last7daysGaps.js

--- last7daysGaps.js
+++ last7daysGaps.js
@@ -1,5 +1,2 @@
-db.gaps.find({
-  "engine.executionDate": {
-    $gte: ISODate("2025-05-12T00:00:00Z")
-  }
-})
+db.getSiblingDB("UHG_Care").Gaps.find({
+  "engine.executionDate": { $gte: ISODate("2025-05-15T00:00:00Z")
